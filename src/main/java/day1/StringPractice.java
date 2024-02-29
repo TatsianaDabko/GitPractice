77470fe8a3068c487d960910705f24aabbed4074
Tatsiana Dabko <dobkotanya08@gmail.com>
2024-02-29 18:15:26 -0500

    modefied file

--- src/main/java/day1/StringPractice.java
+++ src/main/java/day1/StringPractice.java
@@ -3,6 +3,7 @@ package day1;
 public class StringPractice {
     public static void main(String[] args) {
         System.out.println("Hello World!");
-        System.out.println("Java is fun!");
+        String str = "Java is fun";
+        System.out.println(str);
     }
 }
